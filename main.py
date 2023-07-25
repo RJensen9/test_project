b1493392fdd486ce526617387c6e03d71af428b0
Jensen <jen254@csiro.au>
2023-07-25 10:52:30 +1000

    Numerical Functions Update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from hello import hello_world, say_hello
+import numerical_functions as nf
 
 if __name__ == '__main__':
     hello_world()
     name = input('What\'s your name?: ')
     say_hello(name)
+    print(nf.add(1, 1), nf.subtract(5, 4), nf.multiply(5, 5), nf.divide(20, 4))
