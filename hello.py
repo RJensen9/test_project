e977dde6620da6bea19a5083a605373d25aa50ab
Jensen <jen254@csiro.au>
2023-07-25 10:31:22 +1000

    Say Hello Update

--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
 def hello_world():
     print('Hello, World!')
+
+def say_hello(name):
+    print('Hello, {}!'.format(name))
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from hello import hello_world
+from hello import hello_world, say_hello
 
 if __name__ == '__main__':
     hello_world()
+    name = input('What\'s your name?: ')
+    say_hello(name)
